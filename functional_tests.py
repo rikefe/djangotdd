0e47b80134fa7708e9bea4cf6ba46f7643880ddd
rikefe <rikefe65@gmail.com>
2020-11-06 21:51:04 +0000

    Second test failure

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 
+
 browser = webdriver.Firefox()
 #browser = webdriver.Chrome()
 
+# Jane has heard about a cool new online todo app and she goes to check out the homepage
+
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and header mention to-do lists
+
+assert 'To-Do' in browser.title
+
+browser.quit()
