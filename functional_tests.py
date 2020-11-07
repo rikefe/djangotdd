a8c4380d56f7ef586d531b52c5b217974a9cd62b
rikefe <rikefe65@gmail.com>
2020-11-07 22:08:56 +0000

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -26,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1: Buy groceries' for row in rows))
+        self.assertTrue(any(row.text=='1: Buy groceries' for row in rows),"No item in table")
         #There is still a text box inviting jane to add another item.
         # She enters "Book dentist appointment"
 
